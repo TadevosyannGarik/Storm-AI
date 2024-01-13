b5c07c70ed8180120cd3865a6f26da3869163e55
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-13 17:38:54 +0400

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/theme-provider'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,10 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
-            <html lang="en">
-                <body className={inter.className}>
-                    {children}
-                </body>
+            <html lang="en" suppressHydrationWarning>
+                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+                    <body className={inter.className}>
+                        {children}
+                    </body>
+                </ThemeProvider>
             </html>
         </ClerkProvider>
     )
---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Menu, Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+});
+
+export const Navbar = () => {
+    return (
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden" />
+                <Link href="/">
+                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
+                        Storm AI
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <Button size="sm" variant="premium">
+                    Upgrade
+                    <Sparkles className="w-4 h-4 fill-white text-white ml-2" />
+                </Button>
+                <ModeToggle />
+                <UserButton />
+            </div>
+        </div>
+    )
+}
