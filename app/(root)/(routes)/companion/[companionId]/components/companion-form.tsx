1c71fadc67209bd8710eea3b7690ac1edc046f50
TadevosyannGarik <tadevosyan12@gmail.com>
2024-01-13 22:29:41 +0400

    Companion Creat & Edit API

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -12,6 +12,8 @@ import { Select, SelectContent, SelectItem, SelectValue, SelectTrigger } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -56,6 +58,9 @@ const formSchema = z.object({
 });
 
 export const CompanionForm = ({ categories, initialData }: CompanionFormProps) => {
+    const { toast } = useToast();
+    const router = useRouter();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -71,8 +76,28 @@ export const CompanionForm = ({ categories, initialData }: CompanionFormProps) =
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-      };
+        try {
+            if (initialData) {
+                await axios.patch(`/api/companion/${initialData.id}`, values);
+            } else {
+                await axios.post("/api/companion", values);
+            }
+    
+            toast({
+                description: "Success.",
+                duration: 3000,
+            });
+    
+            router.refresh();
+            router.push("/");
+        } catch (error) {
+            toast({
+                variant: "destructive",
+                description: "Something went wrong.",
+                duration: 3000,
+            });
+        }
+    };;
 
     return (
         <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <body className={cn("bg-secondary", inter.className)}>
                     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
                         {children}
+                        <Toaster />
                     </ThemeProvider>
                 </body>
             </html>
